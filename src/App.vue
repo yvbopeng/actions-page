1e150d59dde95dbb31a21a2029ad4ae0f301ec75
蔚勃鹏 <2032806273@qq.com>
2024-08-20 17:00:18 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-const msg = ref('github actions')
+const count = ref(0);
+
+function increase() {
+  count.value++;
+}
 </script>
 
 <template>
-  <div>
-   {{ msg }}
+  <div @click="increase" class="count">
+   {{  count }}
   </div>
 </template>
 
 <style scoped>
-
+.count {
+  color: red;
+  font-size: 50px;
+  cursor: pointer;
+  user-select: none;
+  
+}
 </style>
